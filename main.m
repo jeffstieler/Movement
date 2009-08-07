eab5fb0879efafa4c96348e284e497aaaa2431e6
Jeff Stieler <jeff.m.stieler@gmail.com>
2009-08-07 19:25:35 -0400

    Initial Commit

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  iPhone App Organizer
+//
+//  Created by Jeff Stieler on 8/7/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
